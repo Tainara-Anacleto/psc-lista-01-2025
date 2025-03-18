2017689b78b21b0f8a27c143ad3e9ab56d5f6648
Tainara-Anacleto <taanacleto.pires94@gmail.com>
2025-03-18 15:09:16 -0300

    Add files via upload

---
+++ MediaAritmetica.java
@@ -0,0 +1,16 @@
+public class MediaAritmetica {
+   public static void main(String[] args){
+    double notaUm, notaDois, media;
+
+    notaUm = 6.8;
+    notaDois = 9;
+
+    media = (notaUm + notaDois)/2;
+System.out.println("A média é: " + media);
+
+if(media>=7){
+    System.out.println("Aluno aprovado!");
+}
+  } 
+}   
+
