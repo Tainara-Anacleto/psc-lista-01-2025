aed519014bdecfe27dca28d47340707c1c518dc2
Tainara-Anacleto <taanacleto.pires94@gmail.com>
2025-03-18 14:22:18 -0300

    Add files via upload

---
+++ PecaN.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PecaN {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+       int nuMero = sc.nextInt();
+
+       System.out.println("O número informado foi: " + nuMero);
+       
+       sc.close();
+
+
+       
+    }
+    
+}
